f8610a0cfff433b6e06d1b1befc24bc9151333a9
Dkolomy <Dkolomy@hotmail.com>
2024-05-26 21:46:11 -0400

    Storage: Final

--- src/components/upload/UploadList.jsx
+++ src/components/upload/UploadList.jsx
@@ -4,33 +4,82 @@ import {
   ref,
   uploadBytes,
   uploadBytesResumable,
+  deleteObject,
   getDownloadURL,
   getMetadata,
   listAll,
 } from "firebase/storage";
-import { List, ListItem } from "@mui/material";
+import { Button, IconButton, Link, List, ListItem } from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 const UploadList = () => {
-  const [imageList, setImageList] = useState(false);
+  const [imageList, setImageList] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [openContent, setOpenContent] = useState(false);
   const imageRef = ref(storage, "images/");
 
   const fetchImages = async () => {
+    setLoading(true);
     const result = await listAll(imageRef);
-    setImageList(result.items);
+    let imagesArray = [];
+    result.items.forEach((itemRef) => {
+      getDownloadURL(itemRef).then((url) => {
+        imagesArray.push({
+          name: itemRef.name,
+          url: url,
+        });
+      });
+    });
+    setImageList(imagesArray);
+    setLoading(false);
   };
 
   useEffect(() => {
     fetchImages();
   }, []);
 
+  const handleDelete = async (name) => {
+    const imageRef = ref(storage, "images/" + name);
+    deleteObject(imageRef)
+      .then(() => {
+        console.log(name + " successfully deleted");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    await fetchImages();
+    setOpenContent(false);
+  };
+
   return (
-    <List>
-      {imageList
-        ? imageList.map((item, idx) => (
-            <ListItem key={idx}>{item.name}</ListItem>
-          ))
-        : null}
-    </List>
+    <>
+      <List>
+        {openContent &&
+          imageList.map((item, idx) => (
+            <ListItem key={idx}>
+              <Link target="_blank" color="inherit" href={item.url}>
+                {item.name}
+              </Link>
+              <IconButton
+                onClick={() => handleDelete(item.name)}
+                aria-label="delete"
+              >
+                <DeleteIcon />
+              </IconButton>
+            </ListItem>
+          ))}
+      </List>
+      {loading && <div>loading...</div>}
+      {!loading && (
+        <Button
+          onClick={() => setOpenContent(!openContent)}
+          variant="outlined"
+          color="secondary"
+        >
+          {openContent ? "Hide Content" : "Show Content"}
+        </Button>
+      )}
+    </>
   );
 };
 
